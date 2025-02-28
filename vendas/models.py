07e174cc3250fae78048bc4ee497f23c3388159e
Douglas Silva <douglasitpro@gmail.com>
2025-02-28 15:01:43 -0300

    Criando model

--- vendas/models.py
+++ vendas/models.py
@@ -1,3 +1,10 @@
 from django.db import models
 
-# Create your models here.
+class Venda(models.Model):
+    usuario = models.CharField(max_length=255)
+    total_vendas = models.IntegerField()
+    horario = models.TimeField()
+
+
+    def __str__(self):
+        return f"{self.usuario} - {self.total_vendas} vendas"
