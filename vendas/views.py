542333835a3dabc734c1dbeee7ad6571f434a1b9
Douglas Silva <douglasitpro@gmail.com>
2025-02-28 16:04:26 -0300

    criado views Upload_vendas e ranking_vendas

--- vendas/views.py
+++ vendas/views.py
@@ -1,3 +1,37 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+import csv
+from .models import Venda
+from .forms import UploadFileForm
+from datetime import datetime
 
-# Create your views here.
+from django.db.models import Sum
+
+
+def upload_vendas(request):
+    if request.method == "POST":
+        form = UploadFileForm(request.POST, request.FILES)
+        if form.is_valid():
+            arquivo = request.FILES['arquivo']
+            decoded_file = arquivo.read().decode('utf-8').splitlines()
+            reader = csv.reader(decoded_file)
+            next(reader)
+
+            for row in reader:
+                usuario = row[0]
+                total_vendas = int(row[1])
+                horario = datetime.strptime(row(2), "%H:%H").time()
+
+                Venda.objects.create(usuario=usuario, total_vendas=total_vendas horario=horario)
+
+            return redirect('ranking')
+        
+        else:
+            form = UploadFileForm()
+
+        return render(request, 'upload.html', {'form': form})
+    
+
+def ranking_vendas(request):
+    vendedores = Venda.objects.values('usuario').annotate(total=Sum('total_vendas')).order_by('-total')
+
+    return render(request, 'ranking.html', {'vendedores': vendedores})
